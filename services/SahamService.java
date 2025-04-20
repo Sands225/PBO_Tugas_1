ed3cd0ca1e483d54fd74f1c8857cd66c5eadcfcc
= <=>
2025-04-21 02:30:52 +0800

    added checkCustomerSahamCode to check customer's saham code in SahamService.java

--- services/SahamService.java
+++ services/SahamService.java
@@ -1,5 +1,9 @@
 package services;
 
+import models.Customer;
+import models.CustomerSaham;
+import data.DataStore;
+
 public class SahamService {
     // show all available saham
 
@@ -8,7 +12,20 @@ public class SahamService {
     // buy saham
 
     // sell saham
+    public boolean checkCustomerSahamCode(String sahamCode, Customer customer) {
+        for (CustomerSaham customerSaham: DataStore.customerSaham) {
+            if (customerSaham.getSaham().getCode().equals(sahamCode)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    public void customerSellSaham(String sahamCode, double quantity) {
+//    public boolean checkCustomerSahamQuantity(double sahamQuantity) {
+//
+//    }
+
+    public void customerSellSaham(String sahamCode, double sahamQuantity) {
+
     }
 }
