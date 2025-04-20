90bdbe1ffc06997add66b56602909dd4ecb53456
= <=>
2025-04-21 02:25:35 +0800

    added customerSellSaham in SahamService.java

--- services/SahamService.java
+++ services/SahamService.java
@@ -1,4 +1,14 @@
 package services;
 
 public class SahamService {
+    // show all available saham
+
+    // show all customer's saham
+
+    // buy saham
+
+    // sell saham
+
+    public void customerSellSaham(String sahamCode, double quantity) {
+    }
 }
