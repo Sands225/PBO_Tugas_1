81e661d723a4ba1220a295408a12c33010e22654
= <=>
2025-04-21 02:07:11 +0800

    added customerSahamMenu in CustomerView.java

--- views/CustomerView.java
+++ views/CustomerView.java
@@ -1,6 +1,8 @@
 package views;
 
+import data.DataStore;
 import models.Customer;
+import models.CustomerSaham;
 import utils.Input;
 import views.View;
 
@@ -17,10 +19,33 @@ public class CustomerView {
 
             switch (choice) {
                 case 1:
-//                    customerSahamMenu(customer);
+                    customerSahamMenu(customer);
                     break;
                 case 2:
 //                    customerSBNMenu(customer);
+                    break;
+                case 3:
+                    View view = new View();
+                    view.mainView();
+            }
+        } while (choice < 1 || choice > 3);
+    }
+
+    public void customerSahamMenu(Customer customer) {
+        int choice;
+
+        do {
+            System.out.println("1. Jual Saham");
+            System.out.println("2. Beli Saham");
+            System.out.println("3. Kelar");
+            choice = input.inputNextInt("Masukkan pilihan Anda: ");
+
+            switch (choice) {
+                case 1:
+//                    customerSellSaham(customer);
+                    break;
+                case 2:
+//                    customerBuySahamu(customer);
                     break;
                 case 3:
                     View view = new View();
