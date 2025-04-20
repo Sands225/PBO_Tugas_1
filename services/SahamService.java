8a147759cdb1b87d79ae6d7b9bf969a2eee87ccc
= <=>
2025-04-21 06:34:11 +0800

    updated checkCustomerSahamQuantity and customerSellSaham in SahamService.java

--- services/SahamService.java
+++ services/SahamService.java
@@ -5,27 +5,41 @@ import models.CustomerSaham;
 import data.DataStore;
 
 public class SahamService {
-    // show all available saham
-
-    // show all customer's saham
-
-    // buy saham
-
-    // sell saham
-    public boolean checkCustomerSahamCode(String sahamCode, Customer customer) {
+    public CustomerSaham checkCustomerSahamCode(String sahamCode) {
         for (CustomerSaham customerSaham: DataStore.customerSaham) {
             if (customerSaham.getSaham().getCode().equals(sahamCode)) {
-                return true;
+                return customerSaham;
             }
         }
-        return false;
+        return null;
     }
 
-//    public boolean checkCustomerSahamQuantity(double sahamQuantity) {
-//
-//    }
+    public boolean checkCustomerSahamQuantity(double inputQuantity, double custSahamQuantity) {
+        return inputQuantity > 0 && inputQuantity <= custSahamQuantity;
+    }
 
-    public void customerSellSaham(String sahamCode, double sahamQuantity) {
+    public void customerSellSaham(String sahamCode, double sahamQuantity, Customer customer) {
+        CustomerSaham toRemove = null;
 
+        for (CustomerSaham customerSaham: DataStore.customerSaham) {
+            if (customerSaham.getSaham().getCode().equals(sahamCode)) {
+                customerSaham.setQuantity(customerSaham.getQuantity() - sahamQuantity);
+
+                System.out.println("Selamat! Anda berhasil menjual" + sahamQuantity + " saham " + sahamCode);
+
+                if (customerSaham.getQuantity() > 0) {
+                    System.out.printf("📊 Sisa saham Anda di %s: %.2f lembar\n", sahamCode, customerSaham.getQuantity());
+                } else {
+                    toRemove = customerSaham;
+                }
+            }
+        }
+
+        if (toRemove != null) {
+            DataStore.customerSaham.remove(toRemove);
+            System.out.println("Anda sudah tidak memiliki saham " + sahamCode + " di portofolio Anda.");
+        } else {
+            System.out.println("Anda memiliki "  + sahamCode);
+        }
     }
 }
