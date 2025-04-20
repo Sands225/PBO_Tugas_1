a148ca36a7a185e5b5d24e25831d8b12d3215c2f
= <=>
2025-04-20 14:17:30 +0800

    updated on Input.java

--- utils/Input.java
+++ utils/Input.java
@@ -3,33 +3,26 @@ package utils;
 import java.util.Scanner;
 
 public class Input {
+    private final Scanner scanner = new Scanner(System.in);
+
     public String inputNextLine(String prompt) {
         System.out.print(prompt);
 
-        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        scanner.close();
-
         return input;
     }
 
     public int inputNextInt(String prompt) {
         System.out.print(prompt);
 
-        Scanner scanner = new Scanner(System.in);
         int inputNumber = scanner.nextInt();
-        scanner.close();
-
         return inputNumber;
     }
 
     public double inputNextDouble(String prompt) {
         System.out.print(prompt);
 
-        Scanner scanner = new Scanner(System.in);
         double inputNumber = scanner.nextDouble();
-        scanner.close();
-
         return inputNumber;
     }
 }
