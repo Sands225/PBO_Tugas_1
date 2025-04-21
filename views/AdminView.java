e3d71a5f3480ccac285ea38e8594fbf16f932dcf
BagusEriW <baguseriwidura@gmail.com>
2025-04-21 15:39:35 +0800

    Menambahkan adminMenu method

--- views/AdminView.java
+++ views/AdminView.java
@@ -11,6 +11,24 @@ public class AdminView {
     private final SBNService sbnService = new SBNService();
 
     public void adminMenu() {
+        int choice;
+        do {
+            System.out.println("1. Saham");
+            System.out.println("2. SBN");
+            System.out.println("3. Logout");
+            choice = input.inputNextInt("Masukkan pilihan Anda: ");
 
+            switch (choice) {
+                case 1:
+                    adminSahamMenu();
+                    break;
+                case 2:
+                    adminSBNMenu();
+                    break;
+                case 3:
+                    View view = new View();
+                    view.mainView();
+            }
+        } while (choice < 1 || choice > 3);
     }
 }
