e8e11a9a7114e4e9a2cf2349bbca26219dad28fb
BagusEriW <baguseriwidura@gmail.com>
2025-04-21 15:35:41 +0800

    Menambahkan class private

--- views/AdminView.java
+++ views/AdminView.java
@@ -1,6 +1,15 @@
 package views;
 
+import data.DataStore;
+import models.*;
+import services.*;
+import utils.Input;
+
 public class AdminView {
+    private final Input input = new Input();
+    private final SahamService sahamService = new SahamService();
+    private final SBNService sbnService = new SBNService();
+
     public void adminMenu() {
 
     }
