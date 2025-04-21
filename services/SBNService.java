e842f9ed223aec094d480b61924b7f9c1307138d
= <=>
2025-04-22 06:45:26 +0800

    added checkNominalInvestasi in SBNService.java

--- services/SBNService.java
+++ services/SBNService.java
@@ -1,4 +1,18 @@
 package services;
 
+import models.*;
+
 public class SBNService {
+    public boolean checkNominalInvestasi(SBN sbnToBuy, double nominal) {
+        if (nominal <= 0) {
+            System.out.println("❌ Nominal harus lebih dari 0.");
+            return false;
+        }
+
+        if (nominal > sbnToBuy.getKuotaNasional()) {
+            System.out.printf("❌ Kuota tidak mencukupi. Maksimum pembelian: %.2f\n", sbnToBuy.getKuotaNasional());
+            return false;
+        }
+        return true;
+    }
 }
