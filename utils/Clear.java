98da0756b627458b9b6842aeef2eb91d5fa792f8
= <=>
2025-04-24 10:23:39 +0800

    added Clear.screen() method

---
+++ utils/Clear.java
@@ -0,0 +1,12 @@
+package utils;
+
+
+public class Clear {
+    public static void screen(){
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
