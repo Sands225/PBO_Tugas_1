86af7bf904db574610663e5a065b12a5f9ca7c97
BagusEriW <baguseriwidura@gmail.com>
2025-04-27 14:28:05 +0800

    menambahkan method adminMenu

--- views/AdminView.java
+++ views/AdminView.java
@@ -36,4 +36,26 @@ public class AdminView {
             }
         } while (true);
     }
+
+    public void adminMenu() {
+        int choice;
+        do {
+            System.out.println("1. Saham");
+            System.out.println("2. SBN");
+            System.out.println("3. Logout");
+            choice = input.inputNextInt("Masukkan pilihan Anda: ");
+
+            switch (choice) {
+                case 1:
+                    adminSahamMenu();
+                    break;
+                case 2:
+                    adminSBNMenu();
+                    break;
+                case 3:
+                    View view = new View();
+                    view.mainView();
+            }
+        } while (choice < 1 || choice > 3);
+    }
 }
