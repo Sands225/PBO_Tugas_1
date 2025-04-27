7f517650c437ed6e1c3848d045673616985fab18
BagusEriW <baguseriwidura@gmail.com>
2025-04-27 14:25:22 +0800

    menambahkan methods retry

--- views/AdminView.java
+++ views/AdminView.java
@@ -2,33 +2,38 @@ package views;
 
 import data.DataStore;
 import models.*;
-import services.*;
+import services.SBNService;
+import services.SahamService;
 import utils.Input;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class AdminView {
     private final Input input = new Input();
     private final SahamService sahamService = new SahamService();
     private final SBNService sbnService = new SBNService();
 
-    public void adminMenu() {
+    private boolean retry() {
         int choice;
+
         do {
-            System.out.println("1. Saham");
-            System.out.println("2. SBN");
-            System.out.println("3. Logout");
+            System.out.println("==================================");
+            System.out.println("1. Coba lagi");
+            System.out.println("2. Kembali ke menu sebelumnya");
+            System.out.println("==================================");
             choice = input.inputNextInt("Masukkan pilihan Anda: ");
 
             switch (choice) {
                 case 1:
-                    adminSahamMenu();
-                    break;
+                    return true;
                 case 2:
-                    adminSBNMenu();
+                    return false;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih 1 atau 2.");
                     break;
-                case 3:
-                    View view = new View();
-                    view.mainView();
             }
-        } while (choice < 1 || choice > 3);
+        } while (true);
     }
 }
